bba80dcb37e5a3180443113c86f38dee9fdfa3ba
shalu <shalug123456@gmail.com>
2022-09-20 21:38:04 +0530

    prime check

---
+++ prime.js
@@ -0,0 +1,18 @@
+function checkprime(){
+  let c=0;
+  for(let i=2; i<num;i++){
+    if(num%i==0){
+      c++;
+    }
+  }
+return ;
+  
+}
+
+let x=checkprime(17)
+if(x==0){
+  console.log("Yes,Prime")
+}
+else{
+  console.log("Not,Prime")
+}
